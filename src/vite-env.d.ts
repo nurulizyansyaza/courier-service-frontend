c007fa2f13c55b8759bf63870cf638f315883fa2
Yang Edruce <nurulizyansyaza@gmail.com>
2026-03-17 19:56:22 +0800

    declare module and update readme

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,15 @@
 
 declare const __FRAMEWORK__: 'react' | 'vue' | 'svelte';
 declare const __ENTRY__: string;
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<object, object, unknown>;
+  export default component;
+}
+
+declare module '*.svelte' {
+  import type { Component } from 'svelte';
+  const component: Component;
+  export default component;
+}
